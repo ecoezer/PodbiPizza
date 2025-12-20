4b69e652a2e76bebe0f640d396c97a2c199d93fe
Emrah C. Oezer <58663590+ecoezer@users.noreply.github.com>
2025-12-20 09:58:58 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,8 +7,8 @@ const Footer = () => {
   const [showImpressum, setShowImpressum] = useState(false);
   const allergensRef = React.useRef<HTMLDivElement>(null);
   const impressumRef = React.useRef<HTMLDivElement>(null);
-  const phoneNumber = '+495069899999';
-  const displayNumber = '05069 8989997';
+  const phoneNumber = '+4915224290621';
+  const displayNumber = '01522 429 06 21';
 
   const handleAllergensToggle = () => {
     const newState = !showAllergens;
